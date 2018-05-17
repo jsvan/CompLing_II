794ed2304e45d5630c2c98826020377c569b5c79
JULIAN SCHLENKER_VANECEK <julian.vanecek@gmail.com>
2018-05-17 16:09:58 -0400

    added multithreadng file. Need to test it out.

--- nn.py
+++ nn.py
@@ -2,14 +2,17 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import numpy
-from random import randint
+from random import choice, random
 
-input_size = 30 
-hidden_size = 40      # The number of nodes at the hidden layer
-num_classes = 2       # The number of output classes. In this case, from 0 to 9
-num_epochs = 10         # The number of times entire dataset is trained
+input_size = 1       #
+hidden_size = 5      # The number of nodes at the hidden layer
+num_classes = 2       # The number of output classes. -1, 1
+num_epochs = 20         # The number of times entire dataset is trained
 batch_size = 1       # The size of input data took for one iteration
 learning_rate = 0.01 # The speed of convergence
+num_data=18000
+num_data_d2=num_data/2
+
 
 def toBin(x):
   l=list(bin(int(x))[2:])
@@ -17,72 +20,120 @@ def toBin(x):
     l.insert(0, 0)
   return numpy.array([numpy.int64(int(i)) for i in l])
 
-labels = torch.from_numpy(numpy.array([randint(0,63) for _ in range(5000)])).type(torch.FloatTensor)
-train = torch.from_numpy(numpy.array([toBin(i) for i in labels])).type(torch.FloatTensor)
+
+labels = torch.from_numpy(numpy.array([[0 if j<num_data_d2 else 1] for j in range(num_data)])).type(torch.LongTensor)
+train = torch.from_numpy(numpy.array([[float(j)/float(num_data) for _ in range(input_size)] for j in range(num_data)])).type(torch.FloatTensor)
+
+def tupleToTensor(a):
+    tens = torch.zeros(1, len(a))
+    for i in a:
+        tens[0][i] = i
+    return tens
+
+def categoryToTensor(a):
+    a = numpy.int64(int(a))
+    tens = torch.zeros(1, 1)
+    tens[0][0] = a   # 1 prime 0 not
+  #  tens[0][1] = 1-a # 0 prime 1 not
+    return tens
+
 
 def data_batcher(X, Y, batch_size=25):
-    '''
-    helper function to batch data, batch_size is the size of 
-    the batch.
-    In:
-        X: a matrix of size (num_sample, CONTEXT_SIZE*2)
-        Y: an array of size (num_sample)
-        batches: how many data points are in a batche, default:50
-    Out:
-        a batch of X and Y
-    '''
-    indices = numpy.arange(len(X))
-    # np.random.shuffle(indices)
+    indices = numpy.arange(num_data)
     count = 0
     while count < len(X):
         draw = indices[count:min(count+batch_size, len(X))]
-        yield X[draw, : ], Y[draw]
+        yield X[draw, :], Y[draw]
         count += batch_size
 
 
-class Net(nn.Module):
+class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
-        super(Net, self).__init__()                    # Inherited from the parent class nn.Module
-        self.fc1 = nn.Linear(input_size, hidden_size)  # 1st Full-Connected Layer: 784 (input data) -> 500 (hidden node)
-        self.relu = nn.ReLU()                          # Non-Linear ReLU Layer: max(0,x)
-        self.fc2 = nn.Linear(hidden_size, num_classes) # 2nd Full-Connected Layer: 500 (hidden node) -> 10 (output class)
-    
-    def forward(self, x):                              
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
 
-net = Net(input_size, hidden_size, num_classes)
-criterion = nn.MSELoss()
+        self.i2h = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()                          # Non-Linear ReLU Layer: max(0,x)
+        self.i2o = nn.Linear(hidden_size, num_classes)
+        self.softmax = nn.Softmax(dim =1)
+
+
+    def forward(self, input):
+        hidden = self.i2h(input)
+        out = self.i2o(self.relu(hidden))
+        return self.softmax(out)
+
+net = RNN(input_size, hidden_size, num_classes)
+criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
-for epoch in range(num_epochs):
-    print epoch
-    for x, y in data_batcher(train, labels, 25):   # Load a batch of images with its (index, data, class)
-        x = Variable(torch.stack(x))
-        y = Variable(y)
-        optimizer.zero_grad()
-        out = net.forward(x)
-        #print out, y
-        loss = criterion(out, y)
-        loss.backward()
-        optimizer.step()
+
+def go():
+    net.zero_grad()
+    '''
+    for epoch in range(num_epochs):
+        totLoss=0.0
+        print(epoch)
+        for j in range(len(labels)):
+            x, y = train[j], labels[j]
+       # for x, y in data_batcher(train, labels[j]):
+
+            X=Variable(tupleToTensor(x))
+            out = net.forward(X)
+            Y = Variable(categoryToTensor(y).type(torch.LongTensor))
+            out = out
+            optimizer.zero_grad()
+            q, w = out.max(1)
+            printDots(Y.data[0].numpy()[0]-w.data[0])
+            loss = criterion(out, Y.squeeze()) #Needs a 1d list of correct index. Float -> Long
+            totLoss+=loss
+            loss.backward(retain_graph=True)
+            optimizer.step()
+        print totLoss.data[0]/len(labels)
+    '''
+    for epoch in range(num_epochs):
+        print(epoch)
+        totLoss=0.0
+        for x, y in data_batcher(train, labels, 25):   # Load a batch of images with its (index, data, class)
+            x = Variable(x)
+            y = Variable(y)
+            optimizer.zero_grad()
+            out = net.forward(x)
+
+            loss = criterion(out, y.squeeze())
+            totLoss+=loss
+            loss.backward()
+            optimizer.step()
+
+        print(totLoss.data.numpy()/len(labels))
 
 
-def f(x):
-    train = torch.from_numpy(toBin(x)).type(torch.FloatTensor)
-    out = net.forward(Variable(torch.stack([train])))                             # Forward pass: compute the output class given a image
-    #print 'NEW', x, train.tolist(), out
-    return out
 
-ls = 0
+'''
+
+'''
+go()
+
+ls = 0.0
 n =0
-for j in range(64):
-  n = f(j)
-  print j, n.data[0]
-  ls+= criterion(n, Variable(torch.FloatTensor([j])))
+tf='F'
+med=0.0
+avg=0.0
+print("\n\n\n***TESTING***\n\n\n")
 
-print 1-ls.data[0]/64
+ltest = torch.from_numpy(numpy.array([[0 if j<50 else 1] for j in range(100)])).type(torch.LongTensor)
+ttest = torch.from_numpy(numpy.array([[float(j)/float(100) for _ in range(input_size)] for j in range(100)])).type(torch.FloatTensor)
 
+for x, y in data_batcher(ttest, ltest, 25):
 
+    print('VAR X')
+    X=Variable(x)
+    print(X)
+    print('VAR Y')
+    print(Variable(y).squeeze())
+    out = net.forward(X)
+    loss =criterion(out, Variable(y).squeeze())
+    ls+=loss
+    print("OUT")
+    print(out)
+print(ls/100.0)
---
+++ prepThread.py
@@ -0,0 +1,225 @@
+from nltk import word_tokenize
+from nltk.tag import stanford as stanf #TODO core
+from multiprocessing.pool import ThreadPool
+
+#l is input list of posts
+#tp.map(f, l)
+
+from autocorrect import spell
+from glob import glob
+import pickle
+import user_week_buckets as uwb
+
+from data_utils import *
+
+EXCLUDE = {"Anger","BPD","EatingDisorders","MMFB","StopSelfHarm","SuicideWatch","addiction","alcoholism",\
+			"depression","feelgood","getting_over_it","hardshipmates","mentalhealth","psychoticreddit",\
+			"ptsd","rapecounseling","schizophrenia","socialanxiety","survivorsofabuse","traumatoolbox"}
+TOTAL_LIWC = 18
+TRAINFS = ['umd_reddit_suicidewatch_dataset/reddit_posts/controls/split_80-10-10/TRAIN.txt', 'umd_reddit_suicidewatch_dataset/reddit_posts/sw_users/split_80-10-10/TRAIN.txt']
+TESTFS = ['umd_reddit_suicidewatch_dataset/reddit_posts/controls/split_80-10-10/TEST.txt','umd_reddit_suicidewatch_dataset/reddit_posts/sw_users/split_80-10-10/TEST.txt']
+DEVFS = ['umd_reddit_suicidewatch_dataset/reddit_posts/controls/split_80-10-10/DEV.txt','umd_reddit_suicidewatch_dataset/reddit_posts/sw_users/split_80-10-10/DEV.txt']
+ANNOFS = ['umd_reddit_suicidewatch_dataset/reddit_annotation/crowd.csv','umd_reddit_suicidewatch_dataset/reddit_annotation/expert.csv']
+thread_pool = ThreadPool(processes=8)
+msDict = dict()
+liwc = dict()
+tagger = stanf.StanfordPOSTagger(
+	"/Users/owner/stanford-postagger-full-2018-02-27/models/english-caseless-left3words-distsim.tagger")
+
+
+#[postid, userid, timestamp, subreddit]
+def processDataset(dataFiles,liwcFile,stopFile):
+	print('A')
+	with open(liwcFile,"rb") as lfile:
+		liwc = pickle.load(lfile)
+	allocationDict = makeAllocationDict(TRAINFS, TESTFS, DEVFS, ANNOFS)
+	allText = list()
+	allPosts = list()
+	dataFilenames = list()
+	suicideTimes = dict()
+	for dataFilePtrn in dataFiles:
+		dataFilenames += glob(dataFilePtrn)
+
+	res = thread_pool.map(delegate_file_to_threads, dataFilenames)  # treturn list of size file_count, of tuples (all_text_portion, all_posts_portion, suicide_times_portion)
+	all_text_portions=[]
+	all_posts_portions=[]
+	all_suicide_portions={}
+	for tuple in res:
+		all_text_portions += tuple[0]
+		all_posts_portions += tuple[1]
+		all_suicide_portions.update(tuple[2])
+
+	print('Pickling')
+	with open("allText.p", "wb") as f:
+		pickle.dump(allText, f)
+	with open("allPosts.p", "wb") as f:
+		pickle.dump(allPosts, f)
+	with open("msdict.p", "wb") as f:
+		pickle.dump(msDict, f)
+	with open("suicideTimes.p", "wb") as f:
+		pickle.dump(suicideTimes, f)
+
+	print('B')
+	docTopicVecs = collocateAndLDA(allText,stopFile)
+	ntopics = len(docTopicVecs[0])
+	longVeclen = ntopics + TOTAL_LIWC
+	trainPosts = list()
+	testPosts = list()
+	devPosts = list()
+	devTestPosts = list()
+	mentalHealthVec = [0] * ntopics #represents avg topic space vecotr in mental health
+	totMH = 0
+	subredditVecDict = dict() #Flat list of numbers {subreddit_j -> [funcwrdcts and liwc ... topicSpaceVec]}
+	idx = 0
+	for post in allPosts:
+		if post == "IGNORE":
+			mentalHealthVec = [mentalHealthVec[i]+docTopicVecs[i] for i in range(ntopics)]
+			totMH += 1
+		else:
+			subreddit = post[1]
+			subredditVec,count,words = subredditVecDict.get(subreddit,([0]*longVeclen,0,0))
+			longVec = post[8:8+TOTAL_LIWC]+docTopicVecs[idx]
+			subredditVecDict[subreddit] = ([subredditVec[i]+longVec[i] for i in range(longVeclen)],count+1,words+post[2])
+			post[-5] = docTopicVecs[idx]
+			val,lab = allocationDict.get(post[0],(0,-1))
+			post[-1] = lab
+			if val == 0:
+				trainPosts.append(post)
+			elif val == 1:
+				testPosts.append(post)
+			elif val == 2:
+				devPosts.append(post)
+			else:
+				devTestPosts.append(post)		 
+		idx += 1
+	mentalHealthVec = [mentalHealthVec[i]/totMH for i in range(ntopics)]
+	print('C')
+	for (subreddit, (vec,n,w)) in subredditVecDict.items():
+		subredditVecDict[subreddit] = [vec[i]/w for i in range(TOTAL_LIWC)]+[vec[i]/n for i in range(TOTAL_LIWC,longVeclen)]
+
+	for postList,fname in ((trainPosts,"train.p"),(devPosts,"dev.p"),(devTestPosts,"devTest.p")):
+		userPostDict = dict()
+		for post in postList:
+			userPostDict[post[0]] = userPostDict.get(post[0],list()) + [post]
+		outLabelled = list()
+		outUnlabelled = list()
+		for user in userPostDict:
+			if allocationDict[user][1] == 0:
+				bucketList = uwb.interpret_post_features_by_user(userPostDict[user], suicideTimes, subredditVecDict, mentalHealthVec)
+				outUnlabelled.append([bucket for bucket in bucketList if bucket[0][-1] == 0])
+				outLabelled.append([bucket for bucket in bucketList if bucket[0][-1] == -1])
+			else:
+				outLabelled.append(uwb.interpret_post_features_by_user(userPostDict[user], suicideTimes, subredditVecDict, mentalHealthVec))
+		outTup = (outLabelled,outUnlabelled)
+		with open(fname,"wb") as f:
+			pickle.dump(outTup,f)
+
+	userPostDict = dict()
+	print('D')
+	for post in testPosts:
+		userPostDict[post[0]] = userPostDict.get(post[0],list()) + [post]
+	outList = [uwb.interpret_post_features_by_user(userList,suicideTimes,subredditVecDict,mentalHealthVec) for user,userList in userPostDict.items()]
+	print('Pickling')
+	with open("test.p","wb") as f:
+		pickle.dump(outList,f)
+
+	with open("mentalHealthVec.p","wb") as tp:
+		pickle.dump(mentalHealthVec,tp)
+	with open("subredditVecs.p","wb") as tp:
+		pickle.dump(subredditVecDict,tp)
+	with open("suicideTimes.p","wb") as tp:
+		pickle.dump(suicideTimes,tp)
+
+#[userid,subreddit,totw,totmissp,tot1sg,totpron,totpres,totvrb,[funcwrdcts and liwc],[topicSpaceVec],wkday,hr,timestamp,label]
+def processPostText(post, docFile, tagger, msdict, liwcDict, featureList):
+	wrdList = [spellcheck(wrd.lower(),featureList,msdict) for wrd in word_tokenize(post)]
+	docFile += wrdList
+	docFile.append("$|$")
+	tags = tagger.tag(wrdList)
+	for wrd, tag in tags:
+		if tag[0:1] == "V":
+			featureList[7] += 1
+			if tag in {"VBG","VBP","VBZ"}:
+				featureList[6] += 1
+		elif tag[0:3] == "PRP":
+			featureList[5] += 1
+			if wrd in {"me","my","I","myself","mine"}:
+				featureList[4] += 1
+		elif wrd in liwcDict:
+			themes=liwcDict[wrd]
+			for theme in themes:
+				featureList[8+theme] += 1
+	return featureList
+
+def spellcheck(wrd,lst,msdict):
+	if wrd.isalpha():
+		if wrd in msdict:
+			new = msdict[wrd]
+		else:
+			new = spell(wrd).lower()
+			msdict[wrd] = new
+		if lst:
+			lst[2] += 1
+			if new != wrd:
+				lst[3] += 1
+		return new
+	else:
+		return wrd
+
+
+
+def delegate_file_to_threads(dataFile):
+	global msDict
+	# liwc
+	all_text_portion = [] # wil lbe string, '$|$'
+	all_posts_portion =[] # list of 1 element of either IGNORE or [features]
+	suicide_times_portion = {} # dic from user id to post time?
+	print(dataFile)
+	with open(dataFile, "rU", errors="surrogateescape") as data:
+		for post in data:  # post string, a line from file
+			print('*', end='', flush=True)
+			post = post.strip()
+			if post:
+				try:
+					post = post.split("\t") #post a list of strings (post info)
+					titleLast = post[4][-1:]
+					if titleLast.isalnum(): #i.e. not a punctuation mark:
+						post[4] += "."
+					post[4] = " ".join(post[4:])
+					subreddit = post[3]
+					if subreddit in EXCLUDE:
+						all_text_portion = [spellcheck(wrd.lower(),False,msDict) for wrd in word_tokenize(post[5])]
+						all_text_portion.append("$|$")
+						all_posts_portion.append("IGNORE")
+						if subreddit == "SuicideWatch":
+							suicide_times_portion[post[1]] = suicide_times_portion.get(post[1],list()) + [int(post[2])]
+					else:
+						features = [0]*31
+						features[0] = post[1]
+						features[-2] = int(post[2])
+						features[1] = subreddit
+						features = processPostText(post[4],all_text_portion,tagger,msDict,liwc,features)
+						weekend, daytime = timeToDate(int(post[2]))
+						features[-4] = weekend
+						features[-3] = daytime
+						all_posts_portion.append(features)
+				except Exception as e:
+					print (e)
+
+	return (all_text_portion, all_posts_portion, suicide_times_portion)
+
+
+'''post from TEXT FILE
+  RAW POST
+
+  [post_id]
+  [user_id]
+  [timestamp]
+  [subreddit]
+  [post_title]
+  [post_body]'''
+
+
+if __name__ =='__main__':
+	processDataset(["umd_reddit_suicidewatch_dataset/reddit_posts/controls/*.posts",
+	                "umd_reddit_suicidewatch_dataset/reddit_posts/sw_users/*.posts"], "./liwc.p", "engStops")
