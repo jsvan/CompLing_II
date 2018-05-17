066cb573920200f0a66edc9d985ed60300a6cbe2
JULIAN SCHLENKER_VANECEK <julian.vanecek@gmail.com>
2018-05-17 19:39:26 -0400

    i

--- prepThread.py
+++ prepThread.py
@@ -211,7 +211,7 @@ def delegate_file_to_threads(dataFile):
 						features[-3] = daytime
 						all_posts_portion.append(features)
 				except Exception as e:
-					pass
+					print(e)
 
 			count +=1
 	return (all_text_portion, all_posts_portion, suicide_times_portion)
