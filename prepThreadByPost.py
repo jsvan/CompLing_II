1007a8532706c0138921c409fedeb79ef791e4a0
JULIAN SCHLENKER_VANECEK <julian.vanecek@gmail.com>
2018-05-18 04:11:02 -0400

    byPost printing list sizes after appends

--- prepThreadByPost.py
+++ prepThreadByPost.py
@@ -15,7 +15,6 @@ from data_utils import *
 
 download("averaged_perceptron_tagger")
 download("punkt")
-gc.set_debug(gc.DEBUG_LEAK)
 
 # PATH_TO_STANF = "/home/ubuntu/stanford-postagger-full-2018-02-27/models/english-caseless-left3words-distsim.tagger"
 # PATH_TO_JAR = "/home/ubuntu/stanford-postagger-full-2018-02-27/stanford-postagger-3.9.1.jar"
