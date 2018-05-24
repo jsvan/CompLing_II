82717d9e3eea2423c0a2f02b0c16a48f13a10523
Jo Shoemaker <shoem@umiacs.umd.edu>
2018-05-24 16:59:07 -0400

    lil bug

--- data_utils.py
+++ data_utils.py
@@ -95,6 +95,7 @@ def makeAllocationDict(trainFiles,testFiles,devFiles,annoteFiles):
 	allocationDict = dict() #userid to (int,int): first int for train,test,devtest, or dev, second for label
 	for annoteFile in annoteFiles:
 		with open(annoteFile) as f:
+			f.readline()
 			allocationDict.update({line[0]:int(line[1]) for line in [l.strip().split(",") for l in f]})
 	for trainFile in trainFiles:
 		with open(trainFile) as tfile:
